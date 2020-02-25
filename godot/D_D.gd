4aafc2dd3bd019158bb7d771579073814e1b42e9
Irving Rangel Skasurck <skasurck@gmail.com>
2020-02-24 18:57:41 -0600

    margen y un poco de texto

--- godot/D_D.gd
+++ godot/D_D.gd
@@ -5,7 +5,10 @@ var buffer_salida : RichTextLabel
 
 
 func main():
-	buffer_salida.add_text("Bienvenido a D&D")
+	var mensaje_bienvenida = "Bienvenidos a D&D \n\nEste es un juego de es un juego de rol y fantasía "
+	var mensaje_opciones = "\nLo primero es escojer a tu personaje"
+	buffer_salida.add_text(mensaje_bienvenida)
+	buffer_salida.add_text(mensaje_opciones)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
